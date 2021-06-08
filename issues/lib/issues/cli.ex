4a487ed27a2d174976f559db5a3e827d7e46866a
Jonathon Best <jb@jonathonbest.com>
2021-06-07 21:17:14 -0600

    test git bash commit

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -21,6 +21,21 @@ defmodule Issues.CLI do
     Return a tuple of '{ user, project, count }', or ':help' if help was given
     """
 
-    
+    def parse_args(argv) do
+        parse = OptionParser.parse(argv, switches: [ help: :boolean ], aliases: [ h: :help ])
 
+        case parse do
+        
+        { [ help: true ], _, _ }
+        -> :help
+
+        { _, [ user, project, count ], _ }
+        -> { user, project, String.to_integer(count) }
+
+        {_, [ user, project ], _ }
+        -> { user, project, @default_count }
+
+        _ -> :help
+        end
+    end
 end
